54d319d00d1ac2d5a3858224c7de79eaa59200ef
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-01-31 15:11:30 +0530

    three wbsites together

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from docutils.nodes import image
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -18,4 +18,4 @@ def name(name):
     return render_template('name.html', name=name, image=image)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=5000)
